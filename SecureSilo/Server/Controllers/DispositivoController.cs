2c2cf8b46cd3c8bdf0f0dd039f4a8981e8e8ab6b
Teo Giampietro <teo.giampietro@algoritmo.com.ar>
2021-05-06 16:41:11 -0300

    Controller Dispositivos - Panels

---
+++ SecureSilo/Server/Controllers/DispositivoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SecureSilo.Shared;
+using SecureSilo.Server.Data;
+
+namespace SecureSilo.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DispositivosController : ControllerBase
+    {
+
+        public readonly ApplicationDbContext context;
+        public DispositivosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Dispositivo>>> Get()
+        {
+            return await context.Dispositivos.ToListAsync();
+        }
+        [HttpGet("{id}", Name = "obtenerDispositivo")]
+        public async Task<ActionResult<Dispositivo>> Get(int id)
+        {
+            return await context.Dispositivos.FirstOrDefaultAsync(x => x.Id == id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<Dispositivo>>> Post(Dispositivo dispositivo)
+        {
+            context.Add(dispositivo);
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("obtenerDispositivo", new { id = dispositivo.Id }, dispositivo);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var dispositivo = new Dispositivo { Id = id };
+            context.Remove(dispositivo);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpPut]
+        public async Task<ActionResult> Put(Dispositivo dispositivo)
+        {
+            context.Entry(dispositivo).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+    }
+}
