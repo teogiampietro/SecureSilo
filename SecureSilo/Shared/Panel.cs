4da8ea361685dd738ebb91d2ef9aec48951c86cb
Teo Giampietro <teo.giampietro@algoritmo.com.ar>
2021-05-06 16:44:15 -0300

    Fix namespace error

--- SecureSilo/Shared/Panel.cs
+++ SecureSilo/Shared/Panel.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
-namespace BlazorWebApp.Shared
+namespace SecureSilo.Shared
 {
     public class Panel
     {
--- SecureSilo/Shared/Silo.cs
+++ SecureSilo/Shared/Silo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BlazorWebApp.Shared
+namespace SecureSilo.Shared
 {
     public class Silo
     {
