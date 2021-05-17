593c709f1ecdae1706140ce907d6591d30777453
Teo Giampietro <teo.giampietro@algoritmo.com.ar>
2021-05-17 12:34:50 -0300

    CRUD-Silo

---
+++ SecureSilo/Server/Controllers/SiloController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SecureSilo.Shared;
+using SecureSilo.Server.Data;
+
+namespace SecureSilo.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SilosController : ControllerBase
+    {
+        public readonly ApplicationDbContext context;
+        public SilosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Silo>>> Get()
+        {
+            return await context.Silos.Include("Panel").ToListAsync();
+        }
+        [HttpGet("{id}", Name = "obtenerSilo")]
+        public async Task<ActionResult<Silo>> Get(int id)
+        {
+            return await context.Silos.FirstOrDefaultAsync(x => x.Id == id);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post(Silo silo)
+        {
+            context.Add(silo);
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("obtenerSilos", new { id = silo.Id }, silo);
+        }
+        [HttpPut]
+        public async Task<ActionResult> Put(Silo silo)
+        {
+            context.Entry(silo).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var silo = new Silo { Id = id };
+            context.Remove(silo);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
