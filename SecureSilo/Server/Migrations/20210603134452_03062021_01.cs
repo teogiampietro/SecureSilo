4880822104d46e83c41263fea06a60743b7856c4
Teo Giampietro <teo.giampietro@algoritmo.com.ar>
2021-06-04 18:28:16 -0300

    Included dispositivo to get method

---
+++ SecureSilo/Server/Migrations/20210603134452_03062021_01.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecureSilo.Server.Migrations
+{
+    public partial class _03062021_01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Dispositivos",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Estado",
+                table: "Dispositivos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Dispositivos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Dispositivos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
