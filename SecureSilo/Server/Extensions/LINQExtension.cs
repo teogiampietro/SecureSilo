0af9f582a69ea62be2f432de7bd6a16f8eb5c427
Teo Giampietro <teogiampietro@hotmail.com>
2021-07-17 11:38:39 -0300

    linq extension

---
+++ SecureSilo/Server/Extensions/LINQExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SecureSilo.Server.Extensions
+{
+    public static class LINQExtension 
+    {
+        public static IQueryable<TSource> WhereIf<TSource>(this IQueryable<TSource> source, bool condition, Expression<Func<TSource, bool>> predicate)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+    }
+}
