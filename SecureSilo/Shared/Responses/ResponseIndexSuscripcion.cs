bd75cb007d570351932de410d74260273b376f57
Teo Giampietro <teogiampietro@hotmail.com>
2021-07-17 11:41:20 -0300

    Responses

---
+++ SecureSilo/Shared/Responses/ResponseIndexSuscripcion.cs
@@ -0,0 +1,9 @@
+﻿namespace SecureSilo.Shared
+{
+    public class ResponseIndexSuscripcion
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserMail { get; set; }
+    }
+}
---
+++ SecureSilo/Shared/Responses/ResponseSuscripcion.cs
@@ -0,0 +1,11 @@
+﻿using SecureSilo.Shared.Identity;
+using System.Collections.Generic;
+
+namespace SecureSilo.Shared
+{
+    public class ResponseSuscripcion
+    {
+        public ApplicationUser User { get; set; }
+        public List<Suscripcion> Suscripciones { get; set; }
+    }
+}
